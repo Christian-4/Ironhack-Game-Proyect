f80cfae15fb088752f156d2d1a23f743340bd2e7
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-05 13:57:51 +0100

    Player.js 95% & Obstacles 90%

--- js/game.js
+++ js/game.js
@@ -7,24 +7,46 @@ function Game(canvas) {
 
 Game.prototype.init = function () {
     this.intervalId = setInterval(function () {
+        this.framesCounter++;
         this.draw();
         this.move();
+        if (this.framesCounter % 50 === 0) {
+            this.generateObstacles();
+        }
     }.bind(this), 1000 / this.fps);
 };
 
 Game.prototype.reset = function () {
     this.background = new Background(this);
-    this.npc = new Player(this, 100, 150, );
-    this.player = new Player(this, 100, 500, );
+    this.npc = new Player(this, 100, 150, "images/sprite.png");
+    this.player = new Player(this, 100, 500, "images/sprite.png");
+    this.obstacles = [];
+    this.framesCounter = 0;
 }
+
+Game.prototype.generateObstacles = function () {
+    var arrayObstacles = ["images/obstacle01.png", "images/obstacle02.png", "images/obstacle03.png"];
+    this.obstacles.push(new Obstacle(this,
+        40,
+        40,
+        this.player.y0 + this.player.h - 40 - 5,
+        arrayObstacles[Math.floor(Math.random() * (arrayObstacles.length))]));
+};
 
 Game.prototype.draw = function () {
     this.background.draw();
     this.player.draw();
     this.npc.draw();
+    this.npc.animateImg(6);
+    this.obstacles.forEach(function (obstacle) {
+        obstacle.draw();
+    });
 };
 
 Game.prototype.move = function () {
     this.player.move();
     this.npc.movenpc();
+    this.obstacles.forEach(function (obstacle) {
+        obstacle.move();
+    });
 };
--- js/player.js
+++ js/player.js
@@ -1,33 +1,57 @@
-function Player(game, x, y){
+function Player(game, x, y, img) {
     this.game = game;
     this.x = x;
     this.y = y;
     this.y0 = y;
     this.vy = 1;
+    this.img = new Image();
+    this.img.src = img;
+    this.img.frames = 8;
+    this.img.frameIndex = 0;
+    this.w = 75;
+    this.h = 125;
 };
 
-Player.prototype.draw = function(){
-    this.game.ctx.fillRect(this.x,this.y, 50, 100);
+Player.prototype.draw = function () {
+    this.game.ctx.drawImage(
+        this.img,
+        this.img.frameIndex * Math.floor(this.img.width / this.img.frames),
+        0,
+        Math.floor(this.img.width / this.img.frames),
+        this.img.height,
+        this.x,
+        this.y,
+        this.w,
+        this.h);
 };
+
+Player.prototype.animateImg = function (speed) {
+    if (this.game.framesCounter % speed === 0) {
+        this.img.frameIndex += 1;
+        if (this.img.frameIndex > 7) this.img.frameIndex = 0;
+    }
+}
 
 var RIGHT = 39;
 var SPACE = 32;
 
-Player.prototype.move = function(){
-    document.onkeyup = function(event){
+Player.prototype.move = function () {
+    document.onkeyup = function (event) {
         event.preventDefault();
-        if (event.keyCode === RIGHT)
-        this.x += 10;
+        if (event.keyCode === RIGHT){
+            this.animateImg(1);
+            this.x += 10;
+        }
     }.bind(this);
 
-    document.onkeydown = function(event){
+    document.onkeydown = function (event) {
         event.preventDefault();
         if (event.keyCode === SPACE && this.y == this.y0)
-        this.y -= 80;
+            this.y -= 80;
         this.vy += 1;
     }.bind(this);
 
-    if (this.y >= this.y0){
+    if (this.y >= this.y0) {
         this.vy = 1;
         this.y = this.y0;
     } else {
@@ -35,6 +59,6 @@ Player.prototype.move = function(){
     }
 };
 
-Player.prototype.movenpc = function(){
-    this.x += 0.5; 
+Player.prototype.movenpc = function () {
+    this.x += 0.5;
 }
